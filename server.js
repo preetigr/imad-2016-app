074d5b0732d6ee63cd69ea6432a9f7a18425ea0d
preetigr <preeti4gr@gmail.com>
2016-10-05 02:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function (req, res) {
 }); 
     
 app.get('/article-two',function (req, res) {
-res.send('Article two requested and will be served here');
+res.send('Article two requested and will be served here by gr');
 });
 app.get('/ui/madi.png', function (req, res) {
 app.get('/article-three',function (req, res) {
