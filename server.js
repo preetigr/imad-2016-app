4e2b86d4352566e0f7940105648eaeca4b5d2d5b
preetigr <preeti4gr@gmail.com>
2016-10-10 02:12:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function (req, res) {
-    var articleName = reg.params.articleName;
+    var articleName = req.params.articleName;
 res.send(createTemplate(articles[articleName]));
 }); 
 
