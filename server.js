3e2a7d58d96e554ce0b8da63564dba1440d706c0
preetigr <preeti4gr@gmail.com>
2016-11-14 01:11:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/article-one', function(req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
@@ -18,7 +18,7 @@ app.get('/ui/article-three', function(req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'style1.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
